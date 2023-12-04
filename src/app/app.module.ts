1ef1ee0908d0ee404454a5c35432bccb78227149
Mia <mia.steben@outlook.com>
2023-12-03 19:04:12 -0800

    progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { DefaultTvShowComponent } from './default-tv-show/default-tv-show.component';
+import{HttpClientModule} from '@angular/common/http'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultTvShowComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
